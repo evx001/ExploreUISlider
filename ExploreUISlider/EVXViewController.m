2200f9d29010ec305aa6d4391b6ef0881c9ae16b
evx001 <eviexeris@gmail.com>
2014-06-04 15:30:59 -0400

    init commit plus IBOutlet Label and UIACtion routed

---
+++ ExploreUISlider/EVXViewController.m
@@ -0,0 +1,33 @@
+//
+//  EVXViewController.m
+//  ExploreUISlider
+//
+//  Created by evx on 6/4/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import "EVXViewController.h"
+
+@interface EVXViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *mySlideLabel;
+
+@end
+
+@implementation EVXViewController
+- (IBAction)mySlideAction:(id)sender
+{
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
